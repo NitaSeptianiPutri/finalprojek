c8e60880ee15153732595d23103f648dae0521af
NitaSeptianiPutri <nitaseptiani198@gmail.com>
2021-10-18 21:54:14 +0700

    Nama kelompok:Nita Septiani P,Nurhayati tema web:CompanyProfil

---
+++ resources/views/about.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <include_once ('home.blade.php')>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width-device-width, initial-cscale-1">
+	<title>Coffee Company</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+	
+</head>
+<body>
+
+	<!-- header -->
+	<div class="medsos">
+		<div class="container">
+			<ul>
+				<li><a href="#"><i class="fab fa-facebook"></i></a></li>
+				<li><a href="#"><i class="fab fa-youtube"></i></a></li>
+				<li><a href="#"><i class="fab fa-twitter"></i></i></i></a></li>
+			</ul>
+		</div>
+	</div>
+	<header>
+		<div class="container">
+			<h1><a href="index.html">NUSANTARA COFFEE</a></h1>
+			<ul>
+				<li><a href="index.html">HOME</a></li>
+				<li class="active"><a href="about.html">ABOUT</a></li>
+				<li><a href="service.html">EST</a></li>
+				<li><a href="contact.html">CONTACT</a></li>
+			</ul>
+		</div>
+	</header>
+
+	<!-- label -->
+	<section class="label">
+		<div class="container">
+			<p>Home / About</p>
+		</div>
+	</section>
+
+	<!-- about -->
+	<section class="about">
+		<div class="container">
+			<img src="img/banner.png.jpg" width="100%">
+			<h3>ABOUT</h3>
+			<p>TOP Lovers mungkin kalian sudah tidak asing dengan minuman yang bernama kopi.Tapi kali ini kami akan memperkenalkan kopi yang berasal dari berbagai provisi di Indonesia atau yang biasa lebih di kenal dengan sebutan <strong>kopi Nusantara</strong>, yang tak kalah nikmat dan unik dari kopi-kopi buatan luar negeri.</p>
+			<h3>VISI</h3>
+			<p>1.Memperkenalkan brand street Kopi Nusantara ke suluruh dunia</p>
+			<p>2.Mengembangkan dan melestarikan Kopi Nusantara</p>
+			<p>3.Menjadi selera masyarakat dikala jaman modern</p>
+			<h3>MISI</h3>
+			<p>1.Selalu memberikan kualitas dan cita rasa yang terbaik agar konsumen merasa puas </p>
+			<p>2.Membuka jalan pendistribusian yang baru baik di dalam maupun luar negeri</p>
+	</section>
+
+	
+	<!-- footer -->
+	<footer>
+		<div class="container">
+			<small>Copyright &copy; 2021 -Nita Septiani -All Rights Reserver</small>
+		</div>
+	</footer>
+
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width-device-width, initial-cscale-1">
+	<title>Coffee Company</title>
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+	
+</head>
+<body>
+
+	<!-- header -->
+	<div class="medsos">
+		<div class="container">
+			<ul>
+				<li><a href="#"><i class="fab fa-facebook"></i></a></li>
+				<li><a href="#"><i class="fab fa-youtube"></i></a></li>
+				<li><a href="#"><i class="fab fa-twitter"></i></i></i></a></li>
+			</ul>
+		</div>
+	</div>
+	<header>
+		<div class="container">
+			<h1><a href="index.html">NUSANTARA COFFEE</a></h1>
+			<ul>
+				<li class="active"><a href="index.html">HOME</a></li>
+				<li><a href="about.html">ABOUT</a></li>
+				<li><a href="est.html">EST</a></li>
+				<li><a href="contact.html">CONTACT</a></li>
+			</ul>
+		</div>
+	</header>
+
+	<!-- banner -->
+	<section class="banner">
+		<h2>WELCOME TO MY WEBSITE</h2>
+	</section>
+
+	<!-- about -->
+	<section class="about">
+		<div class="container">
+			<h3>ABOUT</h3>
+			<p>TOP Lovers mungkin kalian sudah tidak asing dengan minuman yang bernama kopi.Tapi kali ini kami akan memperkenalkan kopi yang berasal dari berbagai provisi di Indonesia atau yang biasa lebih di kenal dengan sebutan <strong>kopi Nusantara</strong>, yang tak kalah nikmat dan unik dari kopi-kopi buatan luar negeri.</p>
+	</section>
+
+	<!-- colection -->
+	<section class="colection">
+		<div class="container">
+			<h3>EST</h3>
+			<div class="box">
+				<div class="col-4">
+					<div class="icon"><i class="fas fa-coffee"></i></div>
+					<h4>COFFEE</h4>
+				</div>
+				<div class="col-4">
+					<div class="icon"><i class="fas fa-list"></i></div>
+					<h4>MENU</h4>
+				</div>
+				<div class="col-4">
+					<div class="icon"><i class="fas fa-dollar-sign"></i></div>
+					<h4>HARGA</h4>
+				</div>
+				<div class="col-4">
+					<div class="icon"><i class="fas fa-shipping-fast"></i></div>
+					<h4>PENGIIRMAN</h4>
+				</div>
+			</div>
+
+			
+		</div>
+	</section>
+
+	<!-- footer -->
+	<footer>
+		<div class="container">
+			<small>Copyright &copy; 2021 -Nita Septiani-Nurhayati -All Rights Reserver</small>
+		</div>
+	</footer>
+
+</body>
+</html>
